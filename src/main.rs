16c029ec0ed513cc3fee242571b4de5ba5fb2db2
nmyers217 <nickbmyers217@gmail.com>
2020-06-02 11:35:59 -0500

    day 5

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 #![warn(clippy::all)]
 
+use colored::*;
+
 mod day_01;
 mod day_02;
 mod day_03;
 mod day_04;
+mod day_05;
 
 fn main() {
+    println!("{}", "Day 01".green().bold().underline());
     day_01::solve();
-    println!("================================================================================");
+    println!();
+
+    println!("{}", "Day 02".green().bold().underline());
     day_02::solve();
-    println!("================================================================================");
+    println!();
+
+    println!("{}", "Day 03".green().bold().underline());
     day_03::solve();
-    println!("================================================================================");
+    println!();
+
+    println!("{}", "Day 04".green().bold().underline());
     day_04::solve();
-    println!("================================================================================");
+    println!();
+
+    println!("{}", "Day 05".green().bold().underline());
+    day_05::solve();
+    println!();
 }
